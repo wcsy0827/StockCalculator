4fbe13038dbb66d100914534eae0cf1d6c92934b
Eric Shen <zhshen0827@gmail.com>
2018-06-28 16:37:10 +0800

    簡單買賣計算架構

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBuyPrice = new System.Windows.Forms.TextBox();
+            this.txtBuyAmount = new System.Windows.Forms.TextBox();
+            this.txtTex = new System.Windows.Forms.TextBox();
+            this.txtTotalCost = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBEPoint = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTotalProfit = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtSellAmount = new System.Windows.Forms.TextBox();
+            this.txtSellPrice = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(83, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "價格:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(83, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "張數:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(26, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "券商手續費:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(26, 292);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "買進總成本:";
+            // 
+            // txtBuyPrice
+            // 
+            this.txtBuyPrice.Location = new System.Drawing.Point(153, 123);
+            this.txtBuyPrice.Name = "txtBuyPrice";
+            this.txtBuyPrice.Size = new System.Drawing.Size(106, 22);
+            this.txtBuyPrice.TabIndex = 4;
+            // 
+            // txtBuyAmount
+            // 
+            this.txtBuyAmount.Location = new System.Drawing.Point(153, 161);
+            this.txtBuyAmount.Name = "txtBuyAmount";
+            this.txtBuyAmount.Size = new System.Drawing.Size(106, 22);
+            this.txtBuyAmount.TabIndex = 5;
+            // 
+            // txtTex
+            // 
+            this.txtTex.Location = new System.Drawing.Point(153, 209);
+            this.txtTex.Name = "txtTex";
+            this.txtTex.Size = new System.Drawing.Size(106, 22);
+            this.txtTex.TabIndex = 6;
+            this.txtTex.Text = "千分之1.425";
+            // 
+            // txtTotalCost
+            // 
+            this.txtTotalCost.Location = new System.Drawing.Point(153, 294);
+            this.txtTotalCost.Name = "txtTotalCost";
+            this.txtTotalCost.Size = new System.Drawing.Size(106, 22);
+            this.txtTotalCost.TabIndex = 7;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(153, 374);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(106, 41);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "計算";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(313, 292);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "損平價格:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtBEPoint
+            // 
+            this.txtBEPoint.Location = new System.Drawing.Point(421, 289);
+            this.txtBEPoint.Name = "txtBEPoint";
+            this.txtBEPoint.Size = new System.Drawing.Size(106, 22);
+            this.txtBEPoint.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(421, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 41);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "計算";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtTotalProfit
+            // 
+            this.txtTotalProfit.Location = new System.Drawing.Point(421, 333);
+            this.txtTotalProfit.Name = "txtTotalProfit";
+            this.txtTotalProfit.Size = new System.Drawing.Size(106, 22);
+            this.txtTotalProfit.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(421, 209);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(106, 22);
+            this.textBox4.TabIndex = 17;
+            this.textBox4.Text = "千分之1.425";
+            // 
+            // txtSellAmount
+            // 
+            this.txtSellAmount.Location = new System.Drawing.Point(421, 161);
+            this.txtSellAmount.Name = "txtSellAmount";
+            this.txtSellAmount.Size = new System.Drawing.Size(106, 22);
+            this.txtSellAmount.TabIndex = 16;
+            // 
+            // txtSellPrice
+            // 
+            this.txtSellPrice.Location = new System.Drawing.Point(421, 123);
+            this.txtSellPrice.Name = "txtSellPrice";
+            this.txtSellPrice.Size = new System.Drawing.Size(106, 22);
+            this.txtSellPrice.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(313, 336);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 19);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "賣出獲利:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(294, 207);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(109, 19);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "券商手續費:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label9.Location = new System.Drawing.Point(351, 161);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 19);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "張數:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label10.Location = new System.Drawing.Point(351, 121);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 19);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "價格:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label11.Location = new System.Drawing.Point(119, 88);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 19);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "買進";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label12.Location = new System.Drawing.Point(385, 86);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(47, 19);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "賣出";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(421, 253);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(106, 22);
+            this.textBox7.TabIndex = 25;
+            this.textBox7.Text = "千分之3";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label13.Location = new System.Drawing.Point(294, 251);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(109, 19);
+            this.label13.TabIndex = 24;
+            this.label13.Text = "政府證交稅:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 542);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtBEPoint);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTotalProfit);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtSellAmount);
+            this.Controls.Add(this.txtSellPrice);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtTotalCost);
+            this.Controls.Add(this.txtTex);
+            this.Controls.Add(this.txtBuyAmount);
+            this.Controls.Add(this.txtBuyPrice);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "股票買賣金額計算";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBuyPrice;
+        private System.Windows.Forms.TextBox txtBuyAmount;
+        private System.Windows.Forms.TextBox txtTex;
+        private System.Windows.Forms.TextBox txtTotalCost;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBEPoint;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTotalProfit;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtSellAmount;
+        private System.Windows.Forms.TextBox txtSellPrice;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //計算手續費
+            double fee = 0;
+            fee = double.Parse( this.txtBuyPrice.Text)*double.Parse(this.txtBuyAmount.Text) * 0.001425;
+            if(fee<=20)
+            { fee = 20; };
+
+            double buyprice = double.Parse(this.txtBuyPrice.Text) * 1000;
+
+            this.txtTotalCost.Text = (buyprice * double.Parse(txtBuyAmount.Text) + fee).ToString("0:N0");
+
+            this.txtBEPoint.Text = (double.Parse(this.txtBuyPrice.Text) * (1 - 0.001425) / (1 - 0.001425 - 0.003)).ToString("C");
+
+
+
+
+        }
+    }
+}
