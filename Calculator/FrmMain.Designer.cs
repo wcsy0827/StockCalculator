53ce055d23f23ed22f7403c8decbb4f2af5d5da9
Eric Shen <zhshen0827@gmail.com>
2018-07-16 23:16:59 +0800

    frmMain 完成庫存及成本欄位

--- Calculator/FrmMain.Designer.cs
+++ Calculator/FrmMain.Designer.cs
@@ -36,8 +36,14 @@
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.splitContainer3 = new System.Windows.Forms.SplitContainer();
             this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer4 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer5 = new System.Windows.Forms.SplitContainer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
@@ -48,13 +54,22 @@
             this.splitContainer3.Panel1.SuspendLayout();
             this.splitContainer3.Panel2.SuspendLayout();
             this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer4)).BeginInit();
+            this.splitContainer4.Panel1.SuspendLayout();
+            this.splitContainer4.Panel2.SuspendLayout();
+            this.splitContainer4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer5)).BeginInit();
+            this.splitContainer5.Panel1.SuspendLayout();
+            this.splitContainer5.Panel2.SuspendLayout();
+            this.splitContainer5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // btnBuy
             // 
-            this.btnBuy.Location = new System.Drawing.Point(24, 15);
+            this.btnBuy.Location = new System.Drawing.Point(33, 36);
             this.btnBuy.Name = "btnBuy";
-            this.btnBuy.Size = new System.Drawing.Size(96, 73);
+            this.btnBuy.Size = new System.Drawing.Size(115, 73);
             this.btnBuy.TabIndex = 0;
             this.btnBuy.Text = "買進";
             this.btnBuy.UseVisualStyleBackColor = true;
@@ -67,14 +82,14 @@
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(499, 305);
+            this.dataGridView1.Size = new System.Drawing.Size(561, 159);
             this.dataGridView1.TabIndex = 3;
             // 
             // btnSell
             // 
-            this.btnSell.Location = new System.Drawing.Point(24, 120);
+            this.btnSell.Location = new System.Drawing.Point(33, 141);
             this.btnSell.Name = "btnSell";
-            this.btnSell.Size = new System.Drawing.Size(96, 73);
+            this.btnSell.Size = new System.Drawing.Size(115, 73);
             this.btnSell.TabIndex = 4;
             this.btnSell.Text = "賣出";
             this.btnSell.UseVisualStyleBackColor = true;
@@ -82,29 +97,35 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(24, 230);
+            this.button3.Location = new System.Drawing.Point(33, 251);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(96, 73);
+            this.button3.Size = new System.Drawing.Size(115, 73);
             this.button3.TabIndex = 5;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // splitContainer1
             // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(663, 393);
-            this.splitContainer1.SplitterDistance = 46;
+            this.splitContainer1.Size = new System.Drawing.Size(747, 513);
+            this.splitContainer1.SplitterDistance = 60;
             this.splitContainer1.TabIndex = 6;
             // 
             // splitContainer2
             // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer2.Location = new System.Drawing.Point(0, 0);
             this.splitContainer2.Name = "splitContainer2";
@@ -118,12 +139,13 @@
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
-            this.splitContainer2.Size = new System.Drawing.Size(663, 343);
-            this.splitContainer2.SplitterDistance = 160;
+            this.splitContainer2.Size = new System.Drawing.Size(747, 449);
+            this.splitContainer2.SplitterDistance = 180;
             this.splitContainer2.TabIndex = 0;
             // 
             // splitContainer3
             // 
+            this.splitContainer3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer3.Location = new System.Drawing.Point(0, 0);
             this.splitContainer3.Name = "splitContainer3";
@@ -135,31 +157,107 @@
             // 
             // splitContainer3.Panel2
             // 
-            this.splitContainer3.Panel2.Controls.Add(this.dataGridView1);
-            this.splitContainer3.Size = new System.Drawing.Size(499, 343);
-            this.splitContainer3.SplitterDistance = 34;
+            this.splitContainer3.Panel2.Controls.Add(this.splitContainer4);
+            this.splitContainer3.Size = new System.Drawing.Size(563, 449);
+            this.splitContainer3.SplitterDistance = 35;
             this.splitContainer3.TabIndex = 0;
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(197, 9);
+            this.label1.Location = new System.Drawing.Point(232, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 19);
             this.label1.TabIndex = 0;
             this.label1.Text = "庫存狀況";
             // 
+            // splitContainer4
+            // 
+            this.splitContainer4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer4.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer4.Name = "splitContainer4";
+            this.splitContainer4.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer4.Panel1
+            // 
+            this.splitContainer4.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer4.Panel2
+            // 
+            this.splitContainer4.Panel2.Controls.Add(this.splitContainer5);
+            this.splitContainer4.Size = new System.Drawing.Size(563, 410);
+            this.splitContainer4.SplitterDistance = 161;
+            this.splitContainer4.TabIndex = 4;
+            // 
+            // splitContainer5
+            // 
+            this.splitContainer5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer5.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer5.Name = "splitContainer5";
+            this.splitContainer5.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer5.Panel1
+            // 
+            this.splitContainer5.Panel1.Controls.Add(this.label2);
+            // 
+            // splitContainer5.Panel2
+            // 
+            this.splitContainer5.Panel2.Controls.Add(this.dataGridView2);
+            this.splitContainer5.Size = new System.Drawing.Size(561, 243);
+            this.splitContainer5.SplitterDistance = 39;
+            this.splitContainer5.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(232, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "即時新聞";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(557, 196);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(346, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(8, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = " ";
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(663, 393);
+            this.ClientSize = new System.Drawing.Size(747, 513);
             this.Controls.Add(this.splitContainer1);
             this.Name = "FrmMain";
             this.Text = "FrmMain";
             this.Load += new System.EventHandler(this.FrmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
@@ -172,6 +270,16 @@
             this.splitContainer3.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
             this.splitContainer3.ResumeLayout(false);
+            this.splitContainer4.Panel1.ResumeLayout(false);
+            this.splitContainer4.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer4)).EndInit();
+            this.splitContainer4.ResumeLayout(false);
+            this.splitContainer5.Panel1.ResumeLayout(false);
+            this.splitContainer5.Panel1.PerformLayout();
+            this.splitContainer5.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer5)).EndInit();
+            this.splitContainer5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -186,5 +294,10 @@
         private System.Windows.Forms.SplitContainer splitContainer2;
         private System.Windows.Forms.SplitContainer splitContainer3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer4;
+        private System.Windows.Forms.SplitContainer splitContainer5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Calculator/FrmMain.cs
+++ Calculator/FrmMain.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Calculator
 {
@@ -15,6 +16,7 @@ namespace Calculator
         public FrmMain()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
         }
         double _fees = 0;
         private double fees
@@ -59,11 +61,13 @@ namespace Calculator
 
         private void FrmMain_Load(object sender, EventArgs e)
         {
+            //=====庫存狀況=====
             DBTransactionEntities dc = new DBTransactionEntities();
-            var query = dc.Transaction_history.GroupBy(c => c.stockid).Select(od => new
+            var query = dc.Transaction_history.AsParallel().GroupBy(c => c.stockid).Select(od => new
             {
                 股號 = od.Key,
-                庫存=od.Sum(s=>s.amount),
+                庫存 = od.Sum(s => s.amount),
+                平均成本 = od.Where(bs => bs.buysell == true).Sum(c => c.netincome * (-1)) / od.Where(bs => bs.buysell == true).Sum(a => a.amount)/1000
                
                 
             });
@@ -71,25 +75,22 @@ namespace Calculator
 
 
             this.dataGridView1.DataSource = query.ToArray();
+            dataGridView1.Columns["平均成本"].DefaultCellStyle.Format = "c2";
 
-            
-               
 
-            //int stockid = int.Parse(this.txtstockid2.Text);
-            //DBTransactionEntities dc = new DBTransactionEntities();
-            //int[] arrcost = dc.Buyhistory.Where(s => s.stockid == stockid).Select(c => c.totalcost.Value).ToArray();
-            ////int[] arrtotalcost = dc.Buyhistory.Select(c => c.totalcost).Where(s => s == stockid).ToArray();
-            //decimal[] arramount = dc.Buyhistory.Where(s => s.stockid == stockid).Select(c => c.buyamount.Value).ToArray();
 
-            //decimal sumcost = (decimal)arrcost.Sum();
-            //decimal sumamount = arramount.Sum();
-            //decimal _avgcost = sumcost / sumamount / 1000;
-            //avgcost = (double)_avgcost;
-            
-            //dataGridView2.DataSource = dc.Sellhistory.ToArray();
-            //dataGridView2.Columns["Id"].Visible = false;
-            //dataGridView2.Columns["totalprofit"].DefaultCellStyle.Format = "c0";
-            //dataGridView2.Columns["ROI"].DefaultCellStyle.Format = "P2";
+            //=====RSS Reader=====
+            XDocument rssFeed = XDocument.Load("https://tw.stock.yahoo.com/rss/url/d/e/N1.html");
+            var queryrss = from item in rssFeed.Descendants("item").AsParallel()
+                           select new
+                        {
+                            標題 = item.Element("title").Value,
+                            發布時間 =  item.Element("pubDate").Value,
+                            link = item.Element("link").Value,
+                        };
+            dataGridView2.DataSource = queryrss.ToList();
+            this.dataGridView2.Columns["標題"].Width =300;
+            this.dataGridView2.Columns["發布時間"].Width = 200;
 
 
 
